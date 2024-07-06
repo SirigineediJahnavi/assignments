9638dbbd03f0e309f515e656692ed62bb737de61
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-07-06 13:39:37 +0530

    moved files

--- Booking.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BookingSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  class_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
-  status: { type: String, enum: ['confirmed', 'waitlisted', 'cancelled'], default: 'confirmed' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Booking', BookingSchema);
--- bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { bookClass, cancelBooking, getUserBookings } = require('../controllers/bookingController');
-const router = express.Router();
-
-router.post('/book', bookClass);
-router.post('/cancel', cancelBooking);
-router.get('/bookings', getUserBookings);
-
-module.exports = router;
