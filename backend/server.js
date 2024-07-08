4a2b5d839a5807f522debce1f9878e751078bcf6
Sasidhar Sunkesula <114221715+Sasidhar-Sunkesula@users.noreply.github.com>
2024-07-08 16:18:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://crosspe-assignment.vercel.app",
   })
 );
 app.use(express.json());
